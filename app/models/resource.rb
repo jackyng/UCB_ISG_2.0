7aac4acf44aeb00ddcbe5593df600721f6238252
Jimmy Wu <jimmywu126@gmail.com>
2013-03-13 02:36:58 -0700

    Added extremely basic validation for resources.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,8 @@
 class Resource < ActiveRecord::Base
   belongs_to :node
   attr_accessible :count, :name, :url
+
+  validates :name, :presence => true
+  validates :count, :presence => true
+  validates :url, :presence => true
 end
