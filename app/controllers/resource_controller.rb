d0695c4ef79116abb92bd9383d1edc97f9900f11
Minh <en14vn@gmail.com>
2013-03-14 15:57:40 -0700

    Improve error message

--- app/controllers/resource_controller.rb
+++ app/controllers/resource_controller.rb
@@ -1,22 +1,28 @@
 class ResourceController < ApplicationController
   def create
-    @node = Node.find(params[:node_id])
-    @resource = @node.resources.new(:name => params[:name], :url => params[:url], :count => 0)
-
+    if params[:node_id].nil?
+      redirect_to root_url and return
+    end
     if params[:name].nil? or params[:url].nil?
-      render :create
-      return
+      render :create and return
     end
 
+    @node = Node.find(params[:node_id])
+    @resource = @node.resources.new(:name => params[:name], :url => params[:url], :count => 0)
     if @resource.save
       flash[:notice] = "Resource created"
       flash[:error] = ""
       redirect_to node_path(:id => params[:node_id])
     else
-      flash[:error] = "Please check your name and url {#{params[:name]}} {#{params[:url]}}"
+      if not Resource.find_by_name(params[:name]).blank?
+        flash[:error] = "Another resource with same name already created!"
+      elsif not Resource.find_by_url(params[:url]).blank?
+        flash[:error] = "Another resource with same url already created!"
+      else
+        flash[:error] = "Please check your name '#{params[:name]}' and/or url '#{params[:url]}'"
+      end
       flash[:notice] = ""
     end
-    # redirect_to root_url
   end
 
   def destroy
