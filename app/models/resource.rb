846f668fc0884c32907cc64147f2be8b0ba4bba6
Minh <en14vn@gmail.com>
2013-03-15 22:19:30 -0700

    Resource can now init with count = 0, without having to explicitly declare it anymore

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -5,4 +5,13 @@ class Resource < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   validates :count, :presence => true
   validates :url, :presence => true, :uniqueness => true
+
+  def initialize_with_defaults(attrs = nil, &block)
+    initialize_without_defaults(attrs) do
+      setter = lambda { |key, value| self.send("#{key.to_s}=", value) unless !attrs.nil? && attrs.keys.map(&:to_s).include?(key.to_s) }
+      setter.call('count', 0)
+      yield self if block_given?
+    end
+  end
+  alias_method_chain :initialize, :defaults
 end
