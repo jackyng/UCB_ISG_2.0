32a4d1b7831a1248932c2734ca8bf024237e97ff
Minh <en14vn@gmail.com>
2013-03-15 22:19:53 -0700

    Add validator for unique name among sibling nodes

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,7 +1,21 @@
+class ValidateUniqueNameAmongSiblings < ActiveModel::Validator
+  def validate(record)
+    unless record.parent.nil?
+      record.parent.children.each do |child|
+        if record.name == child.name
+          record.errors[:name] << 'Name must be unique among siblings'
+          return false
+        end
+      end
+    end
+  end
+end
+
 class Node < ActiveRecord::Base
   has_ancestry
   has_many :resources
 
  	attr_accessible :name
   validates :name, :presence => true
+  validates_with ValidateUniqueNameAmongSiblings
 end
