da8700417cc56a8e75c9f463d12b7618ab8e777a
Jimmy Wu <jimmywu126@gmail.com>
2013-03-15 20:19:00 -0700

    Added validation against siblings with same name.

--- app/controllers/node_controller.rb
+++ app/controllers/node_controller.rb
@@ -13,11 +13,21 @@ class NodeController < ApplicationController
 
   def create
     # Data validation
+    # If same name as root node, prevent creation
     if params[:name] == Isg2::Application::ROOT_NODE_NAME
       flash[:error] = 'Error: illegal topic name "' + Isg2::Application::ROOT_NODE_NAME + '".'
       redirect_to node_path(:id => params[:parent])
       return
     end
+    # If name would be the same as one of its siblings, prevent creation
+    parent_node = Node.find(params[:parent])
+    potential_siblings = parent_node.children
+    if potential_siblings.exists?(:name => params[:name])
+      flash[:error] = 'Error: illegal topic name "' + params[:name] + '". Name already belongs to a node at the same level.'
+      redirect_to node_path(:id => params[:parent])
+      return
+    end
+
 
     @node = Node.create(:name => params[:name])
   end
@@ -38,8 +48,17 @@ class NodeController < ApplicationController
 
   def add_child
     # Data validation
+    # If same name as root node, prevent creation
     if params[:name] == Isg2::Application::ROOT_NODE_NAME
       flash[:error] = 'Error: illegal topic name "' + Isg2::Application::ROOT_NODE_NAME + '".'
+      redirect_to node_path(:id => params[:parent])
+      return
+    end
+    # If name would be the same as one of its siblings, prevent creation
+    parent_node = Node.find(params[:parent])
+    potential_siblings = parent_node.children
+    if potential_siblings.exists?(:name => params[:name])
+      flash[:error] = 'Error: illegal topic name "' + params[:name] + '". Name already belongs to a node at the same level.'
       redirect_to node_path(:id => params[:parent])
       return
     end
