3c6afa56225960926319c31fbbcb22af41f934e8
bennyhuynh301 <bennyhuynh301@gmail.com>
2013-03-29 08:24:53 -0700

    Add resource view

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,17 @@
+function displayResource(url) {
+	var content = $("#content");
+	if (content.children().length > 0) {
+		content.children().first().remove();
+	}
+	content.addClass('span8');
+	$('<iframe />', {
+		src: url,
+		width: content.width(),
+		height: 900
+	}).appendTo(content);
+}
+
+
 $(document).ready( function() {
 	$("#my_tree").treeview({control: "#treecontrol"});
 });
