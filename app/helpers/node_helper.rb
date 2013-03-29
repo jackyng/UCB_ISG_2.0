a05faf40b6aa44950eaff3dd8b14fc8766a48f07
bennyhuynh301 <bennyhuynh301@gmail.com>
2013-03-29 11:46:43 -0700

    Add admin view

--- app/helpers/node_helper.rb
+++ app/helpers/node_helper.rb
@@ -1,19 +1,33 @@
 module NodeHelper
-	def display_segment(node)
+	def display_segment(node, isAdmin)
     if (node.depth > 0)     
         html = "<li class=\"closed\">"
     else 
         html = "<li class=\"opened\">"
     end
-    html << "<span><i class=\"icon-book\"></i>#{h(node.name)} </span>"
+    html << "<span><i class=\"icon-book\"></i>#{h(node.name)}</span>"
+    add_child_path = node_add_child_path(:parent => node)
+    remove_child_path = node_destroy_path(:node_id => node.id)
+    add_resource_path = resource_create_path(:node_id => node.id)
+    if (isAdmin) 
+        html << "<span><a href=\"#{h(add_child_path)}\" class=\"tree_func\" data-method=\"get\">Add child</a></span>"
+        if (node.depth > 0)
+            html << "<span><a href=\"#{h(remove_child_path)}\" class=\"tree_func\" data-method=\"get\">Remove child</a></span>"
+        end
+        html << "<span><a href=\"#{h(add_resource_path)}\" class=\"tree_func\" data-method=\"get\">Add resource </a></span>"
+    end
     html << "<ul>"
     node.children.each{|child_node|
-      html << display_segment(child_node)
+      html << display_segment(child_node, isAdmin)
     }
     node.resources.each{|resource|
     	html << "<li>"
         html << "<button onclick=\"displayResource('#{h(resource.url)}')\" class=\"resource\" type=\"button\"><i class=\"icon-file\"></i>#{h(resource.name)}</button>"
-    	html << "</li>"
+    	if (isAdmin)
+            remove_resource_path = resource_destroy_path(:id => resource.id)
+            html << "<span><a href=\"#{h(remove_resource_path)}\" class=\"tree_func\" data-method=\"get\">Remove resource</a></span>"
+        end
+        html << "</li>"
     }
     html << "</ul></li>"
     html.html_safe
