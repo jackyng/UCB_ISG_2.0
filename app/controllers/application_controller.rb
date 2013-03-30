1abcbebdc8a6c86c221a672248d7a9f6db6428a5
Minh <minh.luong@berkeley.edu>
2013-03-30 04:23:09 -0700

    Add new user after logged in and save that user in @current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :get_calnet_info
   private
   def get_calnet_info
-    @login_url = CASClient::Frameworks::Rails::Filter.login_url(self)
-    @calnet_id = session[:cas_user]
-    @user_fullname ||= name_for_calnet_id() unless @calnet_id.nil?
+    unless session[:cas_user].nil?
+      @current_user = User.find_or_create_by_calnetID(session[:cas_user])
+      @user_fullname = name_for_calnet_id()
+    end
+
     @remote_ip = request.remote_ip
+    @login_url = CASClient::Frameworks::Rails::Filter.login_url(self)
   end
 
-  private
   def name_for_calnet_id
     ldap = Net::LDAP.new(
       host: 'ldap.berkeley.edu',
@@ -18,13 +19,13 @@ class ApplicationController < ActionController::Base
     )
     if ldap.bind
       ldap.search(
-        base:         "ou=people,dc=berkeley,dc=edu",
-        filter:       Net::LDAP::Filter.eq( "uid", @calnet_id ),
-        attributes:   %w[ displayName ],
-        return_result:true
+        base:          "ou=people,dc=berkeley,dc=edu",
+        filter:        Net::LDAP::Filter.eq( "uid", @current_user.calnetID.to_s ),
+        attributes:    %w[ displayName ],
+        return_result: true
       ).first.displayName.first
     else
-      flash[:error] = "Can't connect to LDAP"
+      flash[:error] = "Can't connect to LDAP to get user's name"
     end
   end
 end
