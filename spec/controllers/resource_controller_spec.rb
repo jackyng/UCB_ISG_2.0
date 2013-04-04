74931b5e93e946484d17c88b1ec50312da8636ef
Minh <minh.luong@berkeley.edu>
2013-04-04 02:40:25 -0700

    Add spec for non-admins trying to create/destroy resources

--- spec/controllers/resource_controller_spec.rb
+++ spec/controllers/resource_controller_spec.rb
@@ -3,17 +3,15 @@ require 'spec_helper'
 describe ResourceController do
   before(:each) do
     @root = Node.create(:name => Isg2::Application::ROOT_NODE_NAME)
+    @user = User.create(:calnetID => 181758, :isAdmin => true)
+    CASClient::Frameworks::Rails::Filter.fake('181758')
   end
 
-  context "success request" do
-    before(:each) do
-      User.create(:calnetID => 181758, :isAdmin => true)
-      CASClient::Frameworks::Rails::Filter.fake('181758')
-    end
-
-    describe "GET 'resource/create'" do
+  context "admins make good requests:" do
+    describe "GET 'resource/create' -" do
       it "create a new resource under a topic and redirect to that topic path" do
         get 'create', { :name => "169", :url => "169.edu", :node_id => @root.id }
+        flash[:error].should be_blank
         resource = Resource.where(:name => "169", :url => "169.edu")[0]
         resource.should_not be_nil
 
@@ -23,8 +21,8 @@ describe ResourceController do
       end
     end
 
-    describe "GET 'resource/destroy'" do
-      it "should destroy resource and return to the path of the topic containing it before deletion" do
+    describe "GET 'resource/destroy' -" do
+      it "destroy resource and return to the path of the topic containing it before deletion" do
         get 'create', { :name => "169", :url => "169.edu", :node_id => @root.id }
         resource = Resource.where(:name => "169", :url => "169.edu")[0]
 
@@ -36,15 +34,15 @@ describe ResourceController do
     end
   end
 
-  context "bad request" do
-    describe "GET 'resource/create' duplicate resource name" do
+  context "admins make bad request:" do
+    describe "GET 'resource/create' duplicate resource name -" do
       it "return to the form with error message" do
         get 'create', :name => "", :url => '', :node_id => @root.id
         flash[:error].should match /Please check your name '.*' and\/or url '.*'/
       end
     end
     
-    describe "GET 'resource/create' duplicate resource name" do
+    describe "GET 'resource/create' duplicate resource name -" do
       it "return to the form with error message" do
         get 'create', :name => "169", :url => '169.edu', :node_id => @root.id
         get 'create', :name => "169", :url => 'furd169.edu', :node_id => @root.id
@@ -52,11 +50,32 @@ describe ResourceController do
       end
     end
 
-    describe "GET 'resource/create' duplicate resource url" do
+    describe "GET 'resource/create' duplicate resource url -" do
       it "return to the form with error message" do
         get 'create', :name => "169", :url => '169.edu', :node_id => @root.id
         get 'create', :name => "furd169", :url => '169.edu', :node_id => @root.id
         flash[:error].should match /Another resource with same url already created/
+      end
+    end
+  end
+
+  context "non-admins can't make requests to ResourceController:" do
+    describe "GET 'resource/create' -" do
+      it "return to root with error message" do
+        @user.update_attributes(:isAdmin => false)
+        get 'create', :name => "169", :url => '169.edu', :node_id => @root.id
+        flash[:error].should match /Error: You don't have the privilege to perform this action/
+      end
+    end
+
+    describe "GET 'resource/destroy -" do
+      it "return to root with error message" do
+        get 'create', :name => "169", :url => '169.edu', :node_id => @root.id
+        @user.update_attributes(:isAdmin => false)
+
+        resource = Resource.where(:name => "169", :url => "169.edu")[0]
+        get 'destroy', :id => resource.id
+        flash[:error].should match /Error: You don't have the privilege to perform this action/
       end
     end
   end
