d7567a4010bd2d81cf5f9648f8e0dc1f1d6542fc
Minh <minh.luong@berkeley.edu>
2013-04-18 00:51:53 -0700

    Move ldap_lookup to the models

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,23 +13,4 @@ module ApplicationHelper
   	user = User.find(user_id)
   	return user.isAdmin
   end
-
-  def ldap_lookup(calnet_id)
-    unless calnet_id.nil?
-      ldap = Net::LDAP.new(
-        host: 'ldap.berkeley.edu',
-        port: 389
-      )
-      if ldap.bind
-        ldap.search(
-          base:          "ou=people,dc=berkeley,dc=edu",
-          filter:        Net::LDAP::Filter.eq( "uid", calnet_id.to_s ),
-          attributes:    %w[ displayName ],
-          return_result: true
-        ).first.displayName.first
-      else
-        flash[:error] = "Can't connect to LDAP to get user's name"
-      end
-    end
-  end
 end
