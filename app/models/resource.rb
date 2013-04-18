fd20bb5b62fbb62a88e82854c2f1abef29db9b30
Minh <minh.luong@berkeley.edu>
2013-04-18 13:54:15 -0700

    Add attr_accessor for node in Resource

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,6 @@
 class Resource < ActiveRecord::Base
   belongs_to :node
-  attr_accessible :count, :name, :url, :content
+  attr_accessible :node, :count, :name, :url, :content
 
   validates :name, :url, :presence => true, :uniqueness => true
   validates :count, :presence => true
