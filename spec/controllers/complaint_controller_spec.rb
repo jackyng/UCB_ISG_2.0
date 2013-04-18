1a656924315a92100a246b07e56c395365545e77
Minh <minh.luong@berkeley.edu>
2013-04-18 13:56:53 -0700

    Add skeleton for Complaint Controller spec

--- spec/controllers/complaint_controller_spec.rb
+++ spec/controllers/complaint_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe ComplaintController do
+  before(:each) do
+    @root = Node.create(:name => Isg2::Application::ROOT_NODE_NAME)
+    @user = User.create(:calnetID => 181758)
+    CASClient::Frameworks::Rails::Filter.fake('181758')
+  end
 
+  describe "users make good requests:" do
+  end
+
+  describe "users make bad requests:" do
+  end
+
+  describe "non-users can't make requests:" do
+  end
 end
