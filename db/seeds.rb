8589fbf2af42de4687c9e3377277ce27b82bd557
Minh <minh.luong@berkeley.edu>
2013-04-18 00:53:16 -0700

    Update seed file for iter3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
-$root = Node.create(:name => "ISG_root")
+$root = Node.create(name: "ISG_root", description: "This is the root node")
 
 def create_child(level=2, parent=$root)
   return if level <= 0
-  5.times do |i|
+  4.times do |i|
     name = parent == $root ? "t#{i}" : "#{parent.name}.#{i}"
-    child = Node.create(:name => name, :parent => parent)
+    child = Node.create(name: name, description: "This is node '#{name}'", parent: parent)
     create_resources(child)
     create_child(level-1, child)
   end
@@ -19,18 +12,21 @@ end
 
 def create_resources(node)
   3.times do |i|
-    node.resources.create(:name => "r#{node.name[1..-1]}.#{i}", :url => "https://r#{node.name[1..-1]}.#{i}.com/")
+    node.resources.create(
+      :name => "r#{node.name[1..-1]}.#{i}",
+      :url => "https://r#{node.name[1..-1]}.#{i}.com/"
+    )
   end
 end
 
-create_child(3, $root)
+# create_child(5, $root)
 
 # Creating admins
-User.create(:isAdmin => true, :calnetID => 192881, :email => "pathma@eecs.berkeley.edu")
-User.create(:isAdmin => true, :calnetID => 869195, :email => "JackyDNg@gmail.com")
-User.create(:isAdmin => true, :calnetID => 765055, :email => "minh.luong@berkeley.edu")
-User.create(:isAdmin => true, :calnetID => 968746, :email => "bennyhuynh311@berkeley.edu")
-User.create(:isAdmin => true, :calnetID => 948976, :email => "jimmywu126@gmail.com")
+Admin.create(calnetID: 192881, email: "pathma@eecs.berkeley.edu")
+Admin.create(calnetID: 869195, email: "JackyDNg@gmail.com")
+Admin.create(calnetID: 765055, email: "minh.luong@berkeley.edu")
+Admin.create(calnetID: 968746, email: "bennyhuynh311@berkeley.edu")
+Admin.create(calnetID: 948976, email: "jimmywu126@gmail.com")
 
 # Create normal users from the test calnet id's
 ldap = Net::LDAP.new(
@@ -45,8 +41,8 @@ if ldap.bind
     return_result: true
   ).each do |entry|
     calnetID = entry.uid.first.to_i
-    User.create(:calnetID => calnetID)
-    # `echo #{calnetID} >> calnet_test_ids.txt` # If you want to see the whole list of test id's
+    User.create(calnetID: calnetID)
+    `echo #{calnetID} >> calnet_test_ids.txt` # If you want to see the whole list of test id's
   end
 else
   puts "Can't connect to LDAP to get user's name"
