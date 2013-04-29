a46ed8b8f2e3eabc4fa2a5252df7cf014daf7cec
bennyhuynh301 <bennyhuynh301@gmail.com>
2013-04-29 14:30:31 -0700

    Fix the right-click for admin only

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$("#my_tree").treeview({control: "#treecontrol"});
-	$(".iframe").colorbox({iframe:true, width:"80%", height:"80%"});
+	$(".iframe").colorbox({iframe:true, width:"100%", height:"100%"});
   $('#my_tree *').each(function() {
       $(this).tooltip({
         trigger: 'hover',
