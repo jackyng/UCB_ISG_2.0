3d19cfb6fd9c192464000aa160b5693fed92e77f
Jacky Ng <JackyDNg@gmail.com>
2013-05-02 17:20:16 -0700

    Tabs View for Resources

--- app/assets/javascripts/custom.home.js
+++ app/assets/javascripts/custom.home.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 	$("#my_tree").treeview({control: "#treecontrol"});
 	$(".iframe").colorbox({iframe:true, width:"100%", height:"100%"});
-  $('#my_tree *').each(function() {
+    $('#my_tree *').each(function() {
       $(this).tooltip({
         trigger: 'hover',
         placement: 'right',
         delay: { show: 500, hide: 100 }
       }) 
-  });
+    });
+    $('#body').tabs();
 });
 
