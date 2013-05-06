4a3c730309b6c43245f39f91fff72a1f3fecc66a
Minh <minh.luong@berkeley.edu>
2013-05-05 23:34:19 -0700

    Improve admin/index page

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,19 +1,4 @@
 module AdminHelper
-  def displayAddAdminLink(user)
-    html_code = ""
-    if user.email
-      html_code << link_to("Add Admin", admin_create_path(fullname: user.fullname, email: user.email, calnetID: user.calnetID), :method => :post, :title => "This adds the admin")
-    else
-      html_code << "<form class=\"form-inline\" action=\"#{h(admin_create_path)}\" method=\"get\">"
-      html_code << "<input id=\"email\" name=\"email\" type=\"email\" class=\"input-medium\" placeholder=\"Email required!\">"
-      html_code << "<input id=\"fullname\" name=\"fullname\" type=\"hidden\" value=\"#{h(user.fullname)}\">"
-      html_code << "<input id=\"calnetID\" name=\"calnetID\" type=\"hidden\" value=\"#{h(user.calnetID)}\">"
-      html_code << "&nbsp;&nbsp;<button type=\"submit\" class=\"btn btn-small btn-primary disabled\">Save email & add as Admin</button>"
-      html_code << "</form>"
-    end
-    html_code.html_safe
-  end
-
   def displayAddAdminLinkFromLDAP(ldap_entry)
     fullname = ldap_entry.displayName.first
     calnetID = ldap_entry.uid.first
